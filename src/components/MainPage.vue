d0a1ddeeffdb46a05625a0b2b021d3989ec64bd1
정규민 <kkang606@hs.ac.kr>
2024-04-06 23:21:40 +0900

    firstJGM

---
+++ src/components/MainPage.vue
@@ -0,0 +1,179 @@
+<template>
+    <div>
+        <header>
+            <nav id="nav_main">
+                <router-link to="/"
+                    ><img
+                        src="@/assets/logo.png"
+                        alt="로고 이미지"
+                        id="top_logo"
+                /></router-link>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a href="#main_searchCity" class="nav-link">여행지</a>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link"
+                            >고객지원</router-link
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link"
+                            >이용방법</router-link
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link"
+                            >로그인</router-link
+                        >
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <article>
+            <div>
+                <!-- 여행지 검색 구역 -->
+                <div id="main_searchCity">
+                    <h2><b>어디로 여행을 떠나시나요?</b></h2>
+                    <div>
+                        <form
+                            @submit="submit_Main_SearchBox"
+                            id="main_searchBox"
+                        >
+                            <input
+                                v-model="main_searchWord"
+                                type="search"
+                                placeholder="국가명이나 도시명을 검색하세요"
+                                required
+                            /><button
+                                @click="search"
+                                class="search-button"
+                                type="submit"
+                            >
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div id="trip_type">
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item">
+                                <a
+                                    id="wholeCity_word"
+                                    class="nav-link"
+                                    @click="wholeCity"
+                                    >전체</a
+                                >
+                            </li>
+                            <li class="nav-item">
+                                <a
+                                    id="homeCity_word"
+                                    class="nav-link"
+                                    @click="homeCity"
+                                    >국내</a
+                                >
+                            </li>
+                            <li class="nav-item">
+                                <a
+                                    id="foreignCity_word"
+                                    class="nav-link"
+                                    @click="foreignCity"
+                                    >해외</a
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </article>
+        <footer></footer>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            main_searchWord: "",
+        };
+    },
+    methods: {
+        submit_Main_SearchBox() {
+            // 검색어를 서버로 제출하는 메서드
+        },
+    },
+};
+</script>
+
+<style>
+#nav_main {
+    margin: 10px 25px;
+}
+
+#top_logo {
+    float: left;
+    height: 40px;
+}
+
+header .nav-link {
+    color: black !important;
+    cursor: pointer;
+}
+
+header .nav-link:hover {
+    color: aquamarine !important;
+}
+
+img,
+button {
+    cursor: pointer;
+}
+
+#main_searchBox {
+    display: flex;
+    margin-top: 60px;
+}
+
+#main_searchBox > input {
+    width: 600px;
+    height: 60px;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-right: none;
+    border-radius: 8px;
+    margin-left: 30%;
+    font-size: 18px;
+}
+
+@media (max-width: 768px) {
+    #main_searchBox > input {
+        margin-left: 5%;
+    }
+}
+
+#main_searchBox > input:focus {
+    outline: none;
+}
+
+#main_searchBox > button {
+    background-color: white;
+    background-image: url("@/assets/search_btn.png");
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    border: 1px solid #ccc;
+    border-left: none;
+    width: 50px;
+    border-radius: 8px;
+}
+
+#trip_type {
+    margin-top: 60px;
+    font-size: 20px;
+}
+
+#trip_type .nav-link {
+    color: slategray !important;
+    cursor: pointer;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// eslint-disable-next-line no-unused-vars
+import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createApp(App).mount("#app");
