8fcc5fdc4d036b64d5936e77c158e3329bc572c2
root <root@DESKTOP-GAQCF8L>
2024-04-15 20:47:15 +0900

    add bootstrap-vue-3

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,149 +1,131 @@
 <template>
     <div>
-        <article>
+      <article>
+        <div>
+          <!-- 여행지 검색 구역 -->
+          <div id="main_searchCity">
+            <h2><b>어디로 여행을 떠나시나요?</b></h2>
             <div>
-                <!-- 여행지 검색 구역 -->
-                <div id="main_searchCity">
-                    <h2><b>어디로 여행을 떠나시나요?</b></h2>
-                    <div>
-                        <form
-                            @submit="submit_Main_SearchBox"
-                            id="main_searchBox"
-                        >
-                            <input
-                                v-model="main_searchWord"
-                                type="search"
-                                placeholder="국가명이나 도시명을 검색하세요"
-                                required
-                            /><button
-                                @click="search"
-                                class="search-button"
-                                type="submit"
-                            >
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </form>
-                    </div>
-                    <div id="trip_type">
-                        <ul class="nav justify-content-center">
-                            <li class="nav-item">
-                                <a
-                                    id="wholeCity_word"
-                                    class="nav-link"
-                                    @click="wholeCity"
-                                    >전체</a
-                                >
-                            </li>
-                            <li class="nav-item">
-                                <a
-                                    id="homeCity_word"
-                                    class="nav-link"
-                                    @click="homeCity"
-                                    >국내</a
-                                >
-                            </li>
-                            <li class="nav-item">
-                                <a
-                                    id="foreignCity_word"
-                                    class="nav-link"
-                                    @click="foreignCity"
-                                    >해외</a
-                                >
-                            </li>
-                        </ul>
-                    </div>
-                    <div></div>
-                </div>
+                <b-form @submit.prevent="submit_Main_SearchBox" id="main_searchBox">
+                    <input
+                        v-model="main_searchWord"
+                        type="search"
+                        placeholder="국가명이나 도시명을 검색하세요"
+                        required
+                    >
+                    <b-button type="submit" class="search-button">
+                        <i class="fas fa-search"></i>
+                    </b-button>
+                </b-form>
             </div>
-        </article>
-        <footer></footer>
+            <div id="trip_type">
+              <b-nav pills justified>
+                <b-nav-item @click="wholeCity">전체</b-nav-item>
+                <b-nav-item @click="homeCity">국내</b-nav-item>
+                <b-nav-item @click="foreignCity">해외</b-nav-item>
+              </b-nav>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </article>
+      <footer></footer>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     data() {
-        return {
-            main_searchWord: "",
-        };
+      return {
+        main_searchWord: "",
+      };
     },
     methods: {
-        submit_Main_SearchBox() {
-            // 검색어를 서버로 제출하는 메서드
-        },
+      submit_Main_SearchBox() {
+        // 검색어를 서버로 제출하는 메서드
+      },
+      wholeCity() {
+        // 전체 도시에 대한 처리
+      },
+      homeCity() {
+        // 국내 도시에 대한 처리
+      },
+      foreignCity() {
+        // 해외 도시에 대한 처리
+      },
     },
-};
-</script>
-
-<style>
-#nav_main {
-    margin: 10px 25px;
-}
-
-#top_logo {
-    float: left;
-    height: 40px;
-}
-
-header .nav-link {
-    color: black !important;
-    cursor: pointer;
-}
-
-header .nav-link:hover {
-    color: aquamarine !important;
-}
-
-img,
-button {
-    cursor: pointer;
-}
-
-#main_searchBox {
-    display: flex;
-    margin-top: 60px;
-}
-
-#main_searchBox > input {
-    width: 600px;
-    height: 60px;
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-right: none;
-    border-radius: 8px;
-    margin-left: 30%;
-    font-size: 18px;
-}
-
-@media (max-width: 768px) {
-    #main_searchBox > input {
-        margin-left: 5%;
-    }
-}
-
-#main_searchBox > input:focus {
-    outline: none;
-}
-
-#main_searchBox > button {
-    background-color: white;
-    background-image: url("@/assets/search_btn.png");
-    background-size: contain;
-    background-position: center;
-    background-repeat: no-repeat;
-    border: 1px solid #ccc;
-    border-left: none;
-    width: 50px;
-    border-radius: 8px;
-}
-
-#trip_type {
-    margin-top: 60px;
-    font-size: 20px;
-}
-
-#trip_type .nav-link {
-    color: slategray !important;
-    cursor: pointer;
-}
-</style>
+  };
+  </script>
+  
+  <style>
+  #nav_main {
+      margin: 10px 25px;
+  }
+  
+  #top_logo {
+      float: left;
+      height: 40px;
+  }
+  
+  header .nav-link {
+      color: black !important;
+      cursor: pointer;
+  }
+  
+  header .nav-link:hover {
+      color: aquamarine !important;
+  }
+  
+  img,
+  button {
+      cursor: pointer;
+  }
+  
+  #main_searchBox {
+      display: flex;
+      margin-top: 60px;
+  }
+  
+  #main_searchBox > input {
+      width: 600px;
+      height: 60px;
+      padding: 8px;
+      border: 1px solid #ccc;
+      border-right: none;
+      border-radius: 8px;
+      margin-left: 30%;
+      font-size: 18px;
+  }
+  
+  @media (max-width: 768px) {
+      #main_searchBox > input {
+          margin-left: 5%;
+      }
+  }
+  
+  #main_searchBox > input:focus {
+      outline: none;
+  }
+  
+  #main_searchBox > button {
+      background-color: white;
+      background-image: url("@/assets/search_btn.png");
+      background-size: contain;
+      background-position: center;
+      background-repeat: no-repeat;
+      border: 1px solid #ccc;
+      border-left: none;
+      width: 50px;
+      border-radius: 8px;
+  }
+  
+  #trip_type {
+      margin-top: 60px;
+      font-size: 20px;
+  }
+  
+  #trip_type .nav-link {
+      color: slategray !important;
+      cursor: pointer;
+  }
+  </style>
